c85397e52b16efffaafabe37105fbf5e757a3e17
Amanda Thompson <thompson.adg@gmail.com>
2025-10-24 13:07:19 -0700

    Refactor planet_routes to uncomment and restore get_all_planets and get_one_planet functions

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -30,43 +30,46 @@ def create_planet():
     }
 
     return response, 201
-# @planets_bp.get("")
-# def get_all_planets():
-#     planets_response = []
-#     for planet in planets:
-#         planets_response.append(
-#             {
-#                 "id" : planet.id,
-#                 "name" : planet.name,
-#                 "description" : planet.description,
-#                 "moons" : planet.moons,
-#                 "diameter" : planet.diameter
-#             }
-#         )
-#     return planets_response
 
-# @planets_bp.get("/<planet_id>")
-# def get_one_planet(planet_id):
-#     planet = validate_planet(planet_id)
+@planets_bp.get("")
+def get_all_planets():
+    query = db.select(Planet).order_by(Planet.id)
+    planets = db.session.scalars(query)
+    planets_response = []
+    for planet in planets:
+        planets_response.append(
+            {
+                "id" : planet.id,
+                "name" : planet.name,
+                "description" : planet.description,
+                "moons" : planet.moons,
+                "diameter" : planet.diameter
+            }
+        )
+    return planets_response
 
-#     return {
-#                 "id" : planet.id,
-#                 "name" : planet.name,
-#                 "description" : planet.description,
-#                 "moons" : planet.moons,
-#                 "diameter" : planet.diameter
-#             }
+@planets_bp.get("/<planet_id>")
+def get_one_planet(planet_id):
+    planet = validate_planet(planet_id)
 
-# def validate_planet(planet_id):
-#     try:
-#         planet_id = int(planet_id)
-#     except:
-#         response = {"message": f"planet {planet_id} invalid"}
-#         abort(make_response(response, 400))
+    return {
+                "id" : planet.id,
+                "name" : planet.name,
+                "description" : planet.description,
+                "moons" : planet.moons,
+                "diameter" : planet.diameter
+            }
 
-#     for planet in planets:
-#         if planet.id == planet_id:
-#             return planet
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        response = {"message": f"planet {planet_id} invalid"}
+        abort(make_response(response, 400))
 
-#     response = {"message": f"planet {planet_id} not found"}
-#     abort(make_response(response, 404))
+    query = db.select(Planet).where(Planet.id == planet_id)
+    planet = db.session.scalar(query)
+    if not planet:
+        response = {"message": f"planet {planet_id} not found"}
+        abort(make_response(response, 404))
+    return planet
