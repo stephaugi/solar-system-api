70c8cc6a30a4ab4025c96b19c95913415d1317d8
Ekaterna <edbelova@outlook.com>
2025-10-24 12:12:56 -0700

    Add Planet model and database configuration
    - Implemented the Planet model with attributes: id, name, description, moons, and diameter.
    - Established database connection and migration setup in the application.
    - Created routes for adding new planets to the database.

--- app/models/planet.py
+++ app/models/planet.py
@@ -1,21 +1,28 @@
-# 1. Define a `Planet` class with the attributes `id`, `name`, and `description`, and one additional attribute
-# 1. Create a list of `Planet` instances
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
 
-class Planet:
-    def __init__(self, id, name, description, moons, diameter):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.moons = moons
-        self.diameter = diameter
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str]
+    description: Mapped[str]
+    moons: Mapped[int]
+    diameter: Mapped[int]
 
-planets = [
-    Planet(1, "Mercury", "The smallest and closest planet to the Sun with a rocky surface and no moons.", 0, 4879),
-    Planet(2, "Venus", "A rocky planet with a thick, toxic atmosphere and surface temperatures hot enough to melt lead.", 0, 12104),
-    Planet(3, "Earth", "Our home planet, the only known world to support life, with one natural satellite, the Moon.", 1, 12742),
-    Planet(4, "Mars", "The Red Planet, known for its iron oxide surface and the largest volcano in the solar system, Olympus Mons.", 2, 6779),
-    Planet(5, "Jupiter", "The largest planet, a gas giant with a Great Red Spot and dozens of moons including Ganymede.", 95, 139820),
-    Planet(6, "Saturn", "A gas giant famous for its beautiful ring system and many moons, including Titan.", 83, 116460),
-    Planet(7, "Uranus", "An ice giant that rotates on its side, with a pale blue color due to methane in its atmosphere.", 27, 50724),
-    Planet(8, "Neptune", "The farthest known planet, an ice giant with strong winds and a deep blue appearance.", 14, 49244)
-]
+# class Planet:
+#     def __init__(self, id, name, description, moons, diameter):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.moons = moons
+#         self.diameter = diameter
+
+# planets = [
+#     Planet(1, "Mercury", "The smallest and closest planet to the Sun with a rocky surface and no moons.", 0, 4879),
+#     Planet(2, "Venus", "A rocky planet with a thick, toxic atmosphere and surface temperatures hot enough to melt lead.", 0, 12104),
+#     Planet(3, "Earth", "Our home planet, the only known world to support life, with one natural satellite, the Moon.", 1, 12742),
+#     Planet(4, "Mars", "The Red Planet, known for its iron oxide surface and the largest volcano in the solar system, Olympus Mons.", 2, 6779),
+#     Planet(5, "Jupiter", "The largest planet, a gas giant with a Great Red Spot and dozens of moons including Ganymede.", 95, 139820),
+#     Planet(6, "Saturn", "A gas giant famous for its beautiful ring system and many moons, including Titan.", 83, 116460),
+#     Planet(7, "Uranus", "An ice giant that rotates on its side, with a pale blue color due to methane in its atmosphere.", 27, 50724),
+#     Planet(8, "Neptune", "The farthest known planet, an ice giant with strong winds and a deep blue appearance.", 14, 49244)
+# ]
--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,49 +1,72 @@
-# As a client, I want to send a request...
-
-# 1. ...to get all existing `planets`, so that I can see a list of `planets`, with their `id`, `name`, `description`, and other data of the `planet`.
-
-from flask import Blueprint, abort, make_response
-from app.models.planet import planets
+from flask import Blueprint, abort, make_response, request
+from app.models.planet import Planet
+from ..db import db
 
 planets_bp = Blueprint("planet_bp", __name__, url_prefix="/planets")
 
-@planets_bp.get("")
-def get_all_planets():
-    planets_response = []
-    for planet in planets:
-        planets_response.append(
-            {
-                "id" : planet.id,
-                "name" : planet.name,
-                "description" : planet.description,
-                "moons" : planet.moons,
-                "diameter" : planet.diameter
-            }
-        )
-    return planets_response
+@planets_bp.post("")
+def create_planet():
+    request_body = request.get_json()
+    name_from_request_body = request_body["name"]
+    description_from_request_body = request_body["description"]
+    moons_from_request_body = request_body["moons"]
+    diameter_from_request_body = request_body["diameter"]
 
-@planets_bp.get("/<planet_id>")
-def get_one_planet(planet_id):
-    planet = validate_planet(planet_id)
+    new_planet = Planet(
+        name=name_from_request_body, 
+        description=description_from_request_body,
+        moons=moons_from_request_body,
+        diameter=diameter_from_request_body)
+    
+    db.session.add(new_planet)
+    db.session.commit()
 
-    return {
-                "id" : planet.id,
-                "name" : planet.name,
-                "description" : planet.description,
-                "moons" : planet.moons,
-                "diameter" : planet.diameter
-            }
+    response = {
+        "id": new_planet.id,
+        "name": new_planet.name,
+        "description": new_planet.description,
+        "moons": new_planet.moons,
+        "diameter": new_planet.diameter
+    }
 
-def validate_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except:
-        response = {"message": f"planet {planet_id} invalid"}
-        abort(make_response(response, 400))
+    return response, 201
+# @planets_bp.get("")
+# def get_all_planets():
+#     planets_response = []
+#     for planet in planets:
+#         planets_response.append(
+#             {
+#                 "id" : planet.id,
+#                 "name" : planet.name,
+#                 "description" : planet.description,
+#                 "moons" : planet.moons,
+#                 "diameter" : planet.diameter
+#             }
+#         )
+#     return planets_response
 
-    for planet in planets:
-        if planet.id == planet_id:
-            return planet
+# @planets_bp.get("/<planet_id>")
+# def get_one_planet(planet_id):
+#     planet = validate_planet(planet_id)
 
-    response = {"message": f"planet {planet_id} not found"}
-    abort(make_response(response, 404))
+#     return {
+#                 "id" : planet.id,
+#                 "name" : planet.name,
+#                 "description" : planet.description,
+#                 "moons" : planet.moons,
+#                 "diameter" : planet.diameter
+#             }
+
+# def validate_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except:
+#         response = {"message": f"planet {planet_id} invalid"}
+#         abort(make_response(response, 400))
+
+#     for planet in planets:
+#         if planet.id == planet_id:
+#             return planet
+
+#     response = {"message": f"planet {planet_id} not found"}
+#     abort(make_response(response, 404))
